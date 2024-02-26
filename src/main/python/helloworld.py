4cd6e7006b7a62d5efb19b3211b8ee5d0ae0e8aa
Srikanth Duvvuri <srikanthduvvuri@gmail.com>
2024-02-26 20:36:27 +0530

    made changes to clear lint phase

--- src/main/python/helloworld.py
+++ src/main/python/helloworld.py
@@ -1,4 +1,17 @@
+"""
+This is a simple module to demonstrate a hello world function in Python.
+"""
 import sys
 
 def helloworld(out):
+    """
+    Print a hello world message.
+
+    Args:
+        out: Output stream to write the message to.
+    """
+
     out.write("Hello world of Python\n")
+
+# Call the function with a file object
+helloworld(sys.stdout)
